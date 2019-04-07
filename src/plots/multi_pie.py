85f7dd42072cc7f276b8ffb685136e7925b65191
TearsWillFall <osvaldas.vainauskas@gmail.com>
2019-04-07 19:53:29 +0200

    Uso la funcion create_file y creo un archivo tab.

--- src/plots/multi_pie.py
+++ src/plots/multi_pie.py
@@ -1,5 +1,6 @@
 import pygal
 from pygal.style import Style
+from utils.tools import create_tps_file
 
 
 # Creates an entry inside a collection.
@@ -22,6 +23,60 @@ def create_list(source, min_support, link_body, color):
 
     return result
 
+# Creates a HTML file with mutli-pie plot embed in it 
+def create_html_file(go,kegg,min_identity,min_support,plot_file_name,name):
+        html_file = open(f"{name}.html", "w")
+        html_file.write(f"\
+    <!doctype html>\
+    <html>\
+        <head>\
+            <meta charset=\"utf-8\">\
+            <title>{name}</title>\
+            <style>\
+                body {{background-color: #f9f9f9; font-family: Helvetica, Sans-Serif;}}\
+                a {{color: blue; text-decoration: none;}}\
+            </style>\
+        </head>\
+        \
+        <body>\
+            <h1 style=\"text-align: center;\">Functional annotations of {name}</h1>\
+            <div style=\"display: flex;\">\
+                <object type=\"image/svg+xml\"data=\"{plot_file_name}\" height=\"800\"></object>\
+                <div>\
+                    <div>\
+                        <h4>Minimum identity score: {min_identity}</h4>\
+                        <h4>Minimum support score: {min_support}</h4>\
+                        <div style=\"display: flex;\">")
+        if go:
+
+            html_file.write(f"\
+                            <h2>GO:</h2>\
+                            <ul>")
+
+            for go_item in go:
+                html_file.write(f"<li><strong>{go_item['value']}x</strong>\
+                    <a target=\"_blank\" href=\"{go_item['xlink']['href']}\">{go_item['label']}</a></li>")
+            if kegg:
+                html_file.write(f"\
+                                </ul>")
+        if kegg:
+
+            html_file.write(f"\
+                                <h2>KEGG:</h2>\
+                                <ul>")
+
+            for kegg_item in kegg:
+                html_file.write(f"<li><strong>{kegg_item['value']}x</strong>\
+                    <a target=\"_blank\" href=\"{kegg_item['xlink']['href']}\">{kegg_item['label']}</a></li>")
+
+        html_file.write(f"\
+                            </ul>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </body>\
+    </html>")
 
 # Plots the multi pie chart and stats.
 def plot(go_list, kegg_list, min_support, min_identity, name):
@@ -43,6 +98,8 @@ def plot(go_list, kegg_list, min_support, min_identity, name):
     kegg = create_list(kegg_list, min_support,
                        "https://www.genome.jp/dbget-bin/www_bget?",
                        "rgba(68, 108, 179, .6)")
+    create_tps_file(go,kegg,min_support,min_identity,name)
+
     if go:
         multi_pie.add('GO', go)
     if kegg:
@@ -50,56 +107,5 @@ def plot(go_list, kegg_list, min_support, min_identity, name):
 
     plot_file_name = f"{name}-out.svg"
     multi_pie.render_to_file(plot_file_name)
-
-    html_file = open(f"{name}.html", "w")
-    html_file.write(f"\
-<!doctype html>\
-<html>\
-    <head>\
-        <meta charset=\"utf-8\">\
-        <title>{name}</title>\
-        <style>\
-            body {{background-color: #f9f9f9; font-family: Helvetica, Sans-Serif;}}\
-            a {{color: blue; text-decoration: none;}}\
-        </style>\
-    </head>\
-    \
-    <body>\
-        <h1 style=\"text-align: center;\">Functional annotations of {name}</h1>\
-        <div style=\"display: flex;\">\
-            <object type=\"image/svg+xml\"data=\"{plot_file_name}\" height=\"800\"></object>\
-            <div>\
-                <div>\
-                    <h4>Minimum identity score: {min_identity}</h4>\
-                    <h4>Minimum support score: {min_support}</h4>\
-                    <div style=\"display: flex;\">")
-    if go:
-        
-        html_file.write(f"\
-                        <h2>GO:</h2>\
-                        <ul>")
-        
-        for go_item in go:
-            html_file.write(f"<li><strong>{go_item['value']}x</strong>\
-                <a target=\"_blank\" href=\"{go_item['xlink']['href']}\">{go_item['label']}</a></li>")
-        if kegg:
-            html_file.write(f"\
-                            </ul>")
-    if kegg:
-        
-        html_file.write(f"\
-                            <h2>KEGG:</h2>\
-                            <ul>")
-
-        for kegg_item in kegg:
-            html_file.write(f"<li><strong>{kegg_item['value']}x</strong>\
-                <a target=\"_blank\" href=\"{kegg_item['xlink']['href']}\">{kegg_item['label']}</a></li>")
-
-    html_file.write(f"\
-                        </ul>\
-                    </div>\
-                </div>\
-            </div>\
-        </div>\
-    </body>\
-</html>")
+    create_html_file(go,kegg,min_identity,min_support,plot_file_name,name)
+    
